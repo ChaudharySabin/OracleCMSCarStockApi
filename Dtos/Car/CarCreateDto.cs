d13380c70090e4f303512c58028d1e128d90267a
Sabin Chaudhary <chaudharysabin17@gmail.com>
2025-08-03 16:39:57 +1000

    Changed the required error Messages

--- Dtos/Car/CarCreateDto.cs
+++ Dtos/Car/CarCreateDto.cs
@@ -8,19 +8,19 @@ namespace api.Dtos.Car
 {
     public class CarCreateDto
     {
-        [Required]
+        [Required(ErrorMessage = "Make of the car is required.")]
         [StringLength(100)]
         public string Make { get; set; } = string.Empty;
 
-        [Required]
+        [Required(ErrorMessage = "Model of the car is required.")]
         [StringLength(100)]
         public string Model { get; set; } = string.Empty;
 
-        [Required]
+        [Required(ErrorMessage = "Year of the car is required.")]
         [Range(1886, int.MaxValue)]
         public int Year { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Stock of the car is required.")]
         [Range(0, int.MaxValue)]
         public int Stock { get; set; }
     }
