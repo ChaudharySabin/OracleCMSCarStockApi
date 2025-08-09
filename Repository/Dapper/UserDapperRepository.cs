696a3c0195e9e5d9d061837212b1c032b5b1c6ff
Sabin Chaudhary <chaudharysabin17@gmail.com>
2025-08-09 11:23:21 +1000

    Finished Refactoring UserRepository for dapper

--- Repository/Dapper/UserDapperRepository.cs
+++ Repository/Dapper/UserDapperRepository.cs
@@ -13,41 +13,88 @@ namespace api.Repository.Dapper
             _db = db;
         }
 
+        /// <summary>
+        /// Creates a new user in the database.
+        /// </summary>
+        /// <param name="user">The user to create.</param>
+        /// <returns>
+        /// The created user.
+        /// </returns>
         public async Task<User> CreateUserAsync(User user)
         {
-            var sql = "Insert into Users (Name, Email, Phone, DealerId) Values (@Name, @Email, @Phone, @DealerId); Select last_insert_rowid();";
-            var id = await _db.ExecuteScalarAsync<int>(sql, new { user.Name, user.Email, user.Phone, user.DealerId });
+            var sql = "Insert into AspNetUsers (Name, Email, Phone, DealerId, PhoneNumber) Values (@Name, @Email, @Phone, @DealerId, @PhoneNumber); Select last_insert_rowid();";
+            var id = await _db.ExecuteScalarAsync<int>(sql, new
+            {
+                Name = user.Name,
+                Email = user.Email,
+                Phone = user.Phone,
+                DealerId = user.DealerId,
+                PhoneNumber = user.Phone
+            });
             user.Id = id;
             return user;
         }
 
+        /// <summary>
+        /// Deletes a user by its ID.
+        /// </summary>
+        /// <param name="id">The ID of the user to delete.</param>
+        /// <returns>The deleted user, or null if not found.</returns>
+        /// <exception cref="Exception">Thrown when the user could not be deleted.</exception>
         public async Task<User?> DeleteUserAsync(int id)
         {
-            var sql = "Delete from Users where Id = @Id";
-            var affectedRows = await _db.ExecuteAsync(sql, new { Id = id });
-            if (affectedRows == 0)
+            var user = await GetUserByIdAsync(id);
+            if (user == null)
             {
                 return null;
+            }
+
+            var oldConcurrencyStamp = user.ConcurrencyStamp;
+            var sql = "Delete from AspNetUsers where Id = @Id and ConcurrencyStamp = @ConcurrencyStamp";
+            var affectedRows = await _db.ExecuteAsync(sql, new { Id = id, ConcurrencyStamp = oldConcurrencyStamp });
+            if (affectedRows == 0)
+            {
+                throw new Exception("Something went wrong while deleting the user.");
             }
             return await GetUserByIdAsync(id);
         }
 
+
+        /// <summary>
+        /// Retrieves all users from the database.
+        /// </summary>
+        /// <returns>A list of all users.</returns>
         public Task<IEnumerable<User>> GetAllUsersAsync()
         {
             var sql = "Select " +
                 "u.Id as Id, u.Name as Name, u.Email as Email, u.Phone as Phone, u.DealerId, d.Name as DealerName " +
-                "from Users as u Left join Dealers as d on u.DealerId = d.Id";
+                "from AspNetUsers as u Left join Dealers as d on u.DealerId = d.Id";
             return _db.QueryAsync<User>(sql);
         }
 
+
+        /// <summary>
+        /// Retrieves a user by its ID.
+        /// </summary>
+        /// <param name="id">The ID of the user to retrieve.</param>
+        /// <returns>The user with the specified ID, or null if not found.</returns>
         public async Task<User?> GetUserByIdAsync(int id)
         {
             var sql = "Select " +
                 "u.Id as Id, u.Name as Name, u.Email as Email, u.Phone as Phone, u.DealerId, d.Name as DealerName " +
-                "from Users as u Left join Dealers as d on u.DealerId = d.Id where u.Id = @Id";
+                "from AspNetUsers as u Left join Dealers as d on u.DealerId = d.Id where u.Id = @Id";
             return await _db.QuerySingleOrDefaultAsync<User>(sql, new { Id = id });
         }
 
+        /// <summary>
+        /// Updates a user's information.
+        /// </summary>
+        /// <param name="id">The ID of the user to update.</param>
+        /// <param name="username">The new username for the user.</param>
+        /// <param name="fullname">The new full name for the user.</param>
+        /// <param name="email">The new email for the user.</param>
+        /// <param name="phone">The new phone number for the user.</param>
+        /// <returns>The updated user, or null if not found.</returns>
         public async Task<User?> UpdateUserAsync(int id, string? username, string? fullname, string? email, string? phone)
         {
             var user = await GetUserByIdAsync(id);
@@ -57,30 +104,40 @@ namespace api.Repository.Dapper
             }
             var oldConcurrencyStamp = user.ConcurrencyStamp;
             var newConcurrencyStamp = Guid.NewGuid().ToString("D");
-            var sql = "Update Users set UserName = @UserName, Name = @Name, NormalizedUserName = @NormalizedUserName, " +
-                      "Email = @Email, NormalizedEmail = @NormalizedEmail, SecurityStamp = @SecurityStamp, " +
-                      "ConcurrencyStamp = @ConcurrencyStamp where Id = @Id and ConcurrencyStamp = @ConcurrencyStamp";
+            var sql = "Update AspNetUsers set UserName = @UserName, Name = @Name, NormalizedUserName = @NormalizedUserName, " +
+                      "Email = @Email, NormalizedEmail = @NormalizedEmail, Phone = @Phone, PhoneNumber = @PhoneNumber, " +
+                      "ConcurrencyStamp = @NewConcurrencyStamp where Id = @Id and ConcurrencyStamp = @OldConcurrencyStamp";
             var affectedRows = await _db.ExecuteAsync(sql, new
             {
                 Id = id,
                 UserName = username,
                 Name = fullname,
+                NormalizedUserName = username?.ToUpper() ?? string.Empty,
                 Email = email,
-                Phone = phone,
-                NormalizedUserName = username!.ToUpper(),
                 NormalizedEmail = email?.ToUpper(),
-                SecurityStamp = user.SecurityStamp,
-                ConcurrencyStamp = newConcurrencyStamp
+                Phone = phone,
+                PhoneNumber = phone,
+                NewConcurrencyStamp = newConcurrencyStamp,
+                OldConcurrencyStamp = oldConcurrencyStamp
             });
 
 
             if (affectedRows == 0)
             {
-                return null;
+                throw new Exception("Something went wrong while updating the user.");
             }
+
+            //We can also use user.Name = fullname; user.Email = email; user.Phone = phone; user.ConcurrencyStamp = newConcurrencyStamp; to avoid another query
+            //But here we are returning the updated user from the database for simplicity
             return await GetUserByIdAsync(id);
         }
 
+        /// <summary>
+        /// Updates a user's dealer ID.
+        /// </summary>
+        /// <param name="id">The ID of the user to update.</param>
+        /// <param name="dealerId">The new dealer ID for the user.</param>
+        /// <returns>A tuple containing the updated user and the associated dealer, or null if not found.</returns>
         public async Task<(User?, Dealer?)> UpdateUserDealerIdAsync(int id, int dealerId)
         {
             var user = await GetUserByIdAsync(id);
@@ -96,11 +153,11 @@ namespace api.Repository.Dapper
             user.DealerId = dealerId;
             var oldConcurrencyStamp = user.ConcurrencyStamp;
             var newConcurrencyStamp = Guid.NewGuid().ToString("D");
-            var updateSql = "Update Users set DealerId = @DealerId, ConcurrencyStamp = @ConcurrencyStamp where Id = @Id and ConcurrencyStamp = @OldConcurrencyStamp";
-            var affectedRow = await _db.ExecuteAsync(updateSql, new { DealerId = dealerId, Id = id, ConcurrencyStamp = newConcurrencyStamp, OldConcurrencyStamp = oldConcurrencyStamp });
+            var updateSql = "Update AspNetUsers set DealerId = @DealerId, ConcurrencyStamp = @NewConcurrencyStamp where Id = @Id and ConcurrencyStamp = @OldConcurrencyStamp";
+            var affectedRow = await _db.ExecuteAsync(updateSql, new { DealerId = dealerId, Id = id, NewConcurrencyStamp = newConcurrencyStamp, OldConcurrencyStamp = oldConcurrencyStamp });
             if (affectedRow == 0)
             {
-                return (null, null);
+                throw new Exception("Something went wrong while updating the user.");
             }
             return (user, dealer);
         }
